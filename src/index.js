7ca408f32e2781f61be72a0b20a5f1b6f1806bb0
Md_Shahid <mdshahidk0001@gmail.com>
2022-12-10 17:04:05 +0000

    Update 15 files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+var toggal = document.getElementById("switch");
+var button = document.getElementById("button");
+var background = document.getElementById("main-div");
+var text = document.getElementById("text");
+
+var count = 0;
+button.addEventListener("click", function () {
+  if (count % 2 == 0) {
+    button.style.marginLeft = "80%";
+    background.style.backgroundColor = "black";
+    toggal.style.backgroundColor = "white";
+    text.style.color = "white";
+    button.style.transition = "all 1s";
+  } else {
+    button.style.marginLeft = "0%";
+    background.style.backgroundColor = "white";
+    toggal.style.backgroundColor = "white";
+    text.style.color = "black";
+    button.style.transition = "all 1s";
+  }
+  count++;
+});
